384bf5d28a4124faa6d1813ae6f13eb3c651f150
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-10-04 11:41:57 -0300

    logica 2

--- js/routasByRegiao.js
+++ js/routasByRegiao.js
@@ -1,13 +1,51 @@
 // talves usar api para pegar as regioes
 
   
-const Capitais = ['Porto Velho','Manaus','Rio Branco','Campo Grande','Macapá','Brasília','Boa Vista','Cuiabá','Palmas','São Paulo','Teresina','Rio de Janeiro','Belém','Goiânia','Salvador','Florianópolis','São Luís','Maceió','Porto Alegre','Curitiba','Belo Horizonte','Fortaleza','Recife','João Pessoa','Aracaju','Natal' ,'Vitória'];
-const rotasUnidirecionais =[];
+const Capitais = ['Porto Velho','Manaus','Rio Branco','Campo Grande','Macapá',
+'Brasília','Boa Vista','Cuiabá','Palmas','São Paulo','Teresina','Rio de Janeiro',
+'Belém','Goiânia','Salvador','Florianópolis','São Luís','Maceió','Porto Alegre',
+'Curitiba','Belo Horizonte','Fortaleza','Recife','João Pessoa','Aracaju','Natal' ,'Vitória'];
+
+const rotasUnidirecionais =[
+    ['Rio Branco','Macapá'], ['Macapá','Manaus'], ['Belém','Porto Velho']
+    ['Boa Vista','Palmas'] ['Belém','Rio Branco'] ['Palmas','Rio Branco'],
+    ['Boa Vista','Salvador'], ['Maceió','Fortaleza'], ['São Luís','Salvador'], 
+    ['João Pessoa','Recife'], ['Recife','Teresina'], ['Teresina','Natal'],
+    ['Aracaju','Salvador'], ['Natal','Aracaju'], ['Goiânia','Cuiabá'],
+    ['Cuiabá','Campo Grande'], ['Campo Grande','Brasília'], ['Vitória', 'Cuiabá'],
+    ['Vitória','Belo Horizonte'], ['Belo Horizonte','São Paulo'], ['São Paulo','Rio de Janeiro'],
+    ['Rio de Janeiro','Curitiba'], ['Curitiba','Florianópolis'], ['Florianópolis','Porto Alegre']
+];
+
+
+const regioes =[
+    // agregar com capitais as regioes 
+    Centro_Oeste=[],
+    Nordeste = [],
+    Norte= [],
+    Sul= [],
+    Suldeste=[]
+
+]
+    
+
+
+
+ // const  rota1 = 'norte nordeste';
+ // const rota2 = 'centro-Oeste ,Sudeste'
+ // const rota3 = 'Sul , Sudeste'
+
+
+
 function rotasByRegiao (regiao) {
- // usar o map das regioes
+ // usar o map ou funçao para percorrer das regioes
  
  //comparar  regiao e definir a rota 
  
+
+
+
+ //return rota;
 }
 
  // exportar a funçao  routesByRegiao ;
