d01a1899b9b6a4259a87914b7906a7b153273d7a
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-10-04 11:04:56 -0300

    init logica

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Desafio-Ford
 processo seletivo ford
+
+
+Desafio 
+Utilizando esse conhecimento prévio, como desenvolvedor da empresa, sua função é criar um algoritmo que consiga definir o MÍNIMO DE ROTAS que devem ser mapeadas para que se possa chegar a qualquer outra capital através de uma rota mapeada. 
---
+++ js/routasByRegiao.js
@@ -0,0 +1,13 @@
+// talves usar api para pegar as regioes
+
+  
+const Capitais = ['Porto Velho','Manaus','Rio Branco','Campo Grande','Macapá','Brasília','Boa Vista','Cuiabá','Palmas','São Paulo','Teresina','Rio de Janeiro','Belém','Goiânia','Salvador','Florianópolis','São Luís','Maceió','Porto Alegre','Curitiba','Belo Horizonte','Fortaleza','Recife','João Pessoa','Aracaju','Natal' ,'Vitória'];
+const rotasUnidirecionais =[];
+function rotasByRegiao (regiao) {
+ // usar o map das regioes
+ 
+ //comparar  regiao e definir a rota 
+ 
+}
+
+ // exportar a funçao  routesByRegiao ;
